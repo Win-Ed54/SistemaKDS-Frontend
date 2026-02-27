a5c547cb41e909b6f54c1fe588412979c76050d9
Win-Ed54 <edwinfernsv@gmail.com>
2026-02-27 13:08:17 -0600

    feat: Agregar panel de cocina para pruebas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import KitchenDisplay from "./views/KitchenDisplay";
+import WaiterView from "./views/WaiterView";
 
 function App() {
   return (
     <BrowserRouter>
+      {/* 🔹 Navegación simple para pruebas */}
+      <nav className="p-4 bg-gray-800 text-white flex gap-4">
+        <Link to="/kitchen">Cocina</Link>
+        <Link to="/waiter">Mesero</Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<KitchenDisplay />} />
         <Route path="/kitchen" element={<KitchenDisplay />} />
+        <Route path="/waiter" element={<WaiterView />} />
       </Routes>
     </BrowserRouter>
   );
