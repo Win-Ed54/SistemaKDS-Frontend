b18c5120cabf1f569ce57b1862c88234494190de
Win-Ed54 <edwinfernsv@gmail.com>
2026-03-11 13:05:04 -0600

    feat: Crear pagina de login temporal

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,62 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import KitchenDisplay from "./views/KitchenDisplay";
 import WaiterView from "./views/WaiterView";
+import Login from "./pages/Login";
+
+const PrivateRoute = ({ children }) => {
+
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Login />;
+  }
+
+  return children;
+};
 
 function App() {
+
   return (
     <BrowserRouter>
-      {/* Navegación simple para pruebas */}
+
       <nav className="p-4 bg-gray-800 text-white flex gap-4">
         <Link to="/kitchen">Cocina</Link>
         <Link to="/waiter">Mesero</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<KitchenDisplay />} />
-        <Route path="/kitchen" element={<KitchenDisplay />} />
-        <Route path="/waiter" element={<WaiterView />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <KitchenDisplay />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/kitchen"
+          element={
+            <PrivateRoute>
+              <KitchenDisplay />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/waiter"
+          element={
+            <PrivateRoute>
+              <WaiterView />
+            </PrivateRoute>
+          }
+        />
+
       </Routes>
+
     </BrowserRouter>
   );
 }
